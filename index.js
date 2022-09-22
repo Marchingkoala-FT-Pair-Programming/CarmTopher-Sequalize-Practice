85ba9e09a6cb90cc236447fa2ce25e509779c632
Yijin Ro <yri.carmine@gmail.com>
2022-09-22 15:48:29 -0400

    add css file, href is established

--- index.js
+++ index.js
@@ -1,34 +1,78 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const middleware = express.static(path.join(__dirname, 'style'))
+app.use(middleware)
 const { db, Bookmarker, Category } = require('./db/server')
+const PORT = 3000;
+
+// const router = require('./router')
+// app.use('/', router)
 
 app.get('/', (req,res,next)=>{
     res.send('This website is on!')
 })
 
 app.get('/bookmarker', async (req,res,next)=>{
-    const contentBM = await Bookmarker.findAll({
-        include: [
-            Category
-        ]
-    });
+  // below variable contentBM calls for all instances from modules
+  const contentBM = await Bookmarker.findAll({
+    include: [Category],
+  });
+
+  try {
     res.send(`
-    <body>
-        ${
-           contentBM.map( bm => `
-           <div>
-            <h2>Bookmark name : ${bm.name}</h2>
-            <p>URL: ${bm.url}</p>
-            <p>Category ID: ${bm.categoryId}</p>
-            <p>Category: ${bm.category.name}</p>
-           </div>
-           `).join("") 
-        }
-    </body>
-    `)
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+        <link rel="stylesheet" href="style.css">
+        </head>
+        <body>
+        ${contentBM
+          .map(
+            (bm) => `
+        <div class="main">
+            <p>
+            <a href="/bookmarker/${bm.name}">${bm.name} - ${bm.category.name}</a>
+            </p>
+        </div>
+        `
+          )
+          .join("")}
+        
+        </body>
+        </html>
+        `);
+  } catch (error) {
+    next(error);
+  }
 })
 
+app.get('/bookmarker/:name', async (req,res,next)=>{
+// below variable contentBM calls for an instance where the name matches
+// the name requested by our client
 
-app.listen( 3000, ()=>{
+try{
+    const bmName = req.params.name;
+    const contentBM = await Bookmarker.findAll({
+        attribute:['url'],
+        where: {
+          name : bmName
+      },
+    });
+// hmm.. I'm not sure why it's not receiving the correct url address
+// I'm getting undefined... 
+    res.send(`yay! This is a page for ${bmName}`)
+    console.log(contentBM)
+    } catch(error){
+        next(error);
+    }
+})
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(404).send("Something went wrong");
+});
+
+app.listen( PORT, ()=>{
     console.log('Server is live Again!')
 })
---
+++ router/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+// this page will be filled with what's not in the main index.js file
+// app.get files will be transported here & changed to router.get()
+
+module.exports = router
